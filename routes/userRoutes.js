b87b03fc89fa4d9d09157cb2f2db52b210b59731
Farid ullah <faridullah996@gmail.com>
2022-10-29 10:52:12 +0500

    only admin can delete a user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const { admin } = require('../middleware/admin');
 const { auth } = require('../middleware/auth');
 
 router.route('/me').get(auth, userController.me);
 
 router.route('/')
-	.get(userController.getAllUsers)
+	.get(admin, userController.getAllUsers)
 	.post(auth, userController.createUser);
 
 router.route('/:id')
-	.get(userController.getUserById)
-	.put(userController.updateUser)
-	.delete(userController.deleteUser)
+	.delete(admin, userController.deleteUser)
 
 module.exports = router;
